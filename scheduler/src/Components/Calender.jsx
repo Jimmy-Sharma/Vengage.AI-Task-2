df86a3382aa702b7301ca2ed34fc176b976c253b
Prince Sharma <prince786no.1@gmail.com>
2023-12-03 11:43:23 +0530

    saturday changes done

--- scheduler/src/Components/Calender.jsx
+++ scheduler/src/Components/Calender.jsx
@@ -11,51 +11,62 @@ const Calender = () => {
 
     const isSunday = selectedDate.getDay() === 0;
     const isWednesday = selectedDate.getDay() === 3;
+    const isSaturday = selectedDate.getDay() === 6;
 
     const timeOptionsForOtherDays = [
         '8:00 AM', '8:30 AM', '8:45 AM', '9:00 AM',
-        '12:00 PM', '1:00 PM', '3:00 PM', '5:30 PM', '8:30 PM'
+        '12:00 PM', '1:00 PM', '3:00 PM', '5:30 PM', '6:30 PM'
     ];
 
     const timeOptionsForWedDay = [
         '8:00 AM', '8:30 AM', '8:45 AM', '9:00 AM',
-        '12:00 PM', '12:30 PM', '3:00 PM', '5:30 PM', '8:30 PM'
+        '12:00 PM', '12:30 PM', '3:00 PM', '5:30 PM', '6:30 PM'
     ];
 
     let timeOptions = isSunday ? [] : timeOptionsForOtherDays;
-
     timeOptions = isWednesday ? timeOptionsForWedDay : timeOptionsForOtherDays;
 
     const handleSlotClick = (time) => {
+        console.log(isSunday, isWednesday, isSaturday)
         const isNotAvailableForWednesday = isWednesday && time >= '12:30 PM' && time <= '4:30 PM';
+        const isNotAvailableForSaturday = isSaturday && time >= '12:00 PM' && time <= '7:00 PM';
 
-        if (!isWednesday && time >= '12:30 PM' && time <= '1:00 PM') {
+        if (!isWednesday && !isSaturday && time >= '12:30 PM' && time <= '1:00 PM') {
             console.log('Slots not available on clicking on it');
             toast.error(`Selected time slot: ${time} is not available`, {
                 style: {
-                  padding: "20px 25px",
-                  fontWeight: "bold",
-                  borderRadius: "50px",
+                    padding: "20px 25px",
+                    fontWeight: "bold",
+                    borderRadius: "50px",
                 },
-              });
+            });
         } else if (isNotAvailableForWednesday) {
             console.log(`Selected time slot: ${time} is not available`);
             toast.error(`Selected time slot: ${time} is not available`, {
                 style: {
-                  padding: "20px 25px",
-                  fontWeight: "bold",
-                  borderRadius: "50px",
+                    padding: "20px 25px",
+                    fontWeight: "bold",
+                    borderRadius: "50px",
                 },
-              });
+            });
+        } else if (isNotAvailableForSaturday) {
+            console.log(`Selected time slot: ${time} is not available`);
+            toast.error(`Selected time slot: ${time} is not available`, {
+                style: {
+                    padding: "20px 25px",
+                    fontWeight: "bold",
+                    borderRadius: "50px",
+                },
+            });
         } else {
             console.log(`Selected time slot: ${time} is available`);
             toast.success(`Selected time slot: ${time} is available`, {
                 style: {
-                  padding: "20px 25px",
-                  fontWeight: "bold",
-                  borderRadius: "50px",
+                    padding: "20px 25px",
+                    fontWeight: "bold",
+                    borderRadius: "50px",
                 },
-              });
+            });
         }
     };
 
@@ -79,15 +90,15 @@ const Calender = () => {
                 selectedDate && (
                     <div className="slots-section">
                         <h2>Selected Date: {selectedDate.toDateString()}</h2>
-                        {isWednesday ? (
+                        {isWednesday || isSaturday ? (
                             <div>
-                                <h3>Available Time Slots for Wednesday:</h3>
+                                <h3>Available Time Slots</h3>
                                 <div className="time-buttons">
                                     {timeOptionsForWedDay.map((time, index) => (
                                         <button
                                             key={index}
                                             onClick={() => handleSlotClick(time)}
-                                            className={`slot-button ${time >= '12:30 PM' && time <= '4:30 PM' ? 'not-available' : ''}`}
+                                            className={`slot-button ${isWednesday && time >= '12:30 PM' && time <= '4:30 PM' ? 'not-available' :'' } ${isSaturday && time >= '12:00 PM' && time <= '7:00 PM' ? 'not-available-saturday' : ''}`}
                                         >
                                             {time}
                                         </button>
